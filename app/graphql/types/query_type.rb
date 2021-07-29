d81b9265eb6da16b3d5dc60dbed69bdc4de500c8
koji koyama <k.koyama37@gmail.com>
2021-07-29 09:33:11 +0900

    feature: サンプルリゾルバーの設定。

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,16 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :posts, [Types::PostType], null: false
+    def posts
+      Post.all
+    end
+
+    field :post, Types::PostType, null: false do
+      argument :id, Int, required: false
+    end
+    def post(id:)
+      Post.find(id)
     end
   end
 end
