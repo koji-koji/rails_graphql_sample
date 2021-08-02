549acd1b8e16d6b5dda830d54a1c815290f307e5
koji koyama <k.koyama37@gmail.com>
2021-08-02 23:37:16 +0900

    feature: corsの設定。

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,38 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'example.com'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
+
+
+# config.middleware.use "Rack::Cors" do
 #   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#     origins '*'
+#     resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :options]
+#   end
+# end
+
+
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   # Insert CORS request headers for API responses
+#   allow do
+#     # Access-Control-Allow-Origin: (Origin に書かれたものをそのまま返す)
+#     origins '*'
+
+#     # /api 以下の URL に対してのみ、CORS 対応
+#     # Access-Control-Allow-Methods: GET, POST, OPTIONS
+#     # Access-Control-Allow-Headers: (OPTIONS に対してのみ Access-Control-Request-Headers に書かれたものをそのまま返す)
+#     # Access-Control-Max-Age: 3600
+#     resource '/api/*',
+#              :methods => [:get, :post, :options],
+#              :headers => :any,
+#              :max_age => 3600
 #   end
 # end
